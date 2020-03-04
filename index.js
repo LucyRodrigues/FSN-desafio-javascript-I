3357543a0951a450f4a3b1415ba089ff47efc2eb
LucyRodrigues <lucy.rodriguez.fontana@gmail.com>
2020-03-04 10:51:09 -0300

    minhas tentativas

--- index.js
+++ index.js
@@ -3,3 +3,68 @@ const alunosDaEscola=[{nome:"Henrique",notas:[],cursos:[],faltas:5},{nome:"Edson
 
 
 // implementação
+
+//NÃO ESTÁ FUNCIONANDO TOTALMENTE
+function adicionarAluno(nome) {
+    let alunoNome = {
+      nome: nome
+    };
+    if (alunosDaEscola.nome == nome) {
+      console.log(`O aluno ${nome} já consta em nosso cadastro.`);
+    } else {
+      alunosDaEscola.push(alunoNome);
+      console.log(`O aluno ${nome} foi cadastrado com sucesso!`); //PRECISA CORRIGIR AQUI
+    }
+  }
+  
+  //PARECE ESTAR FUNCIONANDO
+   function listarAlunos() {
+     for (let i = 0; i < alunosDaEscola.length; i++) {
+       console.log("------------------------------");
+       console.log("Nome: ", alunosDaEscola[i].nome);
+       console.log("Notas: ", alunosDaEscola[i].notas);
+      console.log("Cursos: ", alunosDaEscola[i].cursos);
+       console.log("Faltas: ", alunosDaEscola[i].faltas);
+     }
+   }
+  
+  //INCOMPLETO
+  function buscarAluno(nome, alunosDaEscola) {
+    for (i = 0; i < alunosDaEscola.length; i++) {
+      if (alunosDaEscola[i].nome === nome) {
+        console.log(alunosDaEscola[i]);
+         } else {
+           console.log("Este aluno não consta em nosso cadastro.");
+      }
+    }
+  }
+  
+  //NÃO ESTÁ FUNCIONANDO DIREITO
+  function matricularAluno(aluno, nomeDoCurso) {
+    if (alunosDaEscola.nome === aluno) {
+      alunosDaEscola.push(cursos.nomeDoCurso);
+      alunosDaEscola.push(cursos.dataMatricula);
+    }
+  }
+  
+  //FUNÇÃO APLICAR FALTA - NÃO FUNCIONA NADA
+  function aplicarFalta(aluno) {
+    for (i = 0; i < alunosDaEscola.length; i++) {
+      if (alunosDaEscola[i].nome === aluno) {
+        return alunosDaEscola.faltas + 1;
+      }
+      console.log(`Você recebeu 1 falta e tem um total de ${}.`);
+    }
+  }
+  
+  //NÃO ESTÁ CORRETO AINDA
+  function aplicarNota(alunosDaEscola.nome.notas) {
+    if(alunosDaEscola.cursos == " "){
+      alunosDaEscola.notas.push(7, 9, 10);
+      console.log("Notas aplicadas com sucesso.");
+    } else {
+      console.log("Este aluno não está cadastrado em nenhum curso.");
+    }
+  }
+
+  //ÚLTIMA FUNÇÃO NÃO CONSEGUI FAZER A TEMPO
